ffda0d08d5d884bb38d3bdaaf92c7e07f682339b
Boboge <50315826+avgstuBoboge@users.noreply.github.com>
2021-06-04 22:20:00 +0800

    Update 最小费用最大流-dinic.cpp

--- graph/最小费用最大流-dinic.cpp
+++ graph/最小费用最大流-dinic.cpp
@@ -1,118 +1,83 @@
-#include<cstdio>
-#include<algorithm>
-#include<queue>
-
-using namespace std;
-
-const int inf = 0x3f3f3f3f;
-
-//quick read
-template<typename T>
-inline void read(T &x) {
-    int s = 1;
-    x = 0;
-    char ch = getchar();
-    while (ch < '0' || ch > '9') {
-        if (ch == '-') s = -1;
-        ch = getchar();
-    }
-    while (ch >= '0' && ch <= '9') {
-        x = (x << 3) + (x << 1) + (ch ^ 48);
-        ch = getchar();
-    }
-    x *= s;
+namespace MCMF
+{
+const int MAXN = ___, MAXM = ___, INF = 0x7fffffff;
+int head[MAXN], cnt = 1;
+struct Edge
+{
+    int to, w, c, next;
+} edges[MAXM * 2];
+inline void add(int from, int to, int w, int c)
+{
+    edges[++cnt] = {to, w, c, head[from]};
+    head[from] = cnt;
 }
-
-template<typename T, typename... Args>
-inline void read(T &x, Args &... args) {
-    read(x);
-    read(args...);
+inline void addEdge(int from, int to, int w, int c)
+{
+    add(from, to, w, c);
+    add(to, from, 0, -c);
 }
-
-const int maxn = 5000 + 5;
-const int maxe = 50000 * 2 + 5;
-
-int num = 0;
-int head[maxn];
-struct node {
-    int v, flow, dis;
-    int next;
-} edge[maxe];
-
-inline void addedge(int x, int y, int w, int dis) {
-    edge[num].v = y;
-    edge[num].flow = w;
-    edge[num].dis = dis;
-    edge[num].next = head[x];
-    head[x] = num++;
-}
-
-inline void add2edge(int x, int y, int w, int dis) {
-    addedge(x, y, w, dis);
-    addedge(y, x, 0, -dis);
-}
-
-int flow[maxn], dis[maxn];
-int pre[maxn], last[maxn];
-bool vis[maxn];
-
-bool spfa(int n, int s, int t) {
-    for (int i = 1; i <= n; ++i) flow[i] = dis[i] = inf, vis[i] = false;
-    vis[s] = true;
+int s, t, dis[MAXN], cur[MAXN];
+bool inq[MAXN], vis[MAXN];
+queue<int> Q;
+bool SPFA()
+{
+    while (!Q.empty())
+        Q.pop();
+    copy(head, head + MAXN, cur);
+    fill(dis, dis + MAXN, INF);
     dis[s] = 0;
-    pre[t] = -1;
-    queue<int> q;
-    q.push(s);
-    while (!q.empty()) {
-        int u = q.front();
-        q.pop();
-        vis[u] = false;
-        for (int i = head[u]; i != -1; i = edge[i].next) {
-            int v = edge[i].v;
-            if (edge[i].flow && dis[v] > dis[u] + edge[i].dis) {
-                dis[v] = dis[u] + edge[i].dis;
-                pre[v] = u;
-                last[v] = i;
-                flow[v] = min(flow[u], edge[i].flow);
-                if (!vis[v]) {
-                    vis[v] = true;
-                    q.push(v);
+    Q.push(s);
+    while (!Q.empty())
+    {
+        int p = Q.front();
+        Q.pop();
+        inq[p] = 0;
+        for (int e = head[p]; e != 0; e = edges[e].next)
+        {
+            int to = edges[e].to, vol = edges[e].w;
+            if (vol > 0 && dis[to] > dis[p] + edges[e].c)
+            {
+                dis[to] = dis[p] + edges[e].c;
+                if (!inq[to])
+                {
+                    Q.push(to);
+                    inq[to] = 1;
                 }
             }
         }
     }
-    return pre[t] != -1;
+    return dis[t] != INF;
 }
-
-pair<int, int> dinic(int n, int s, int t) {
-    int maxflow = 0, mincost = 0;
-    while (spfa(n, s, t)) {
-        int u = t;
-        maxflow += flow[u];
-        mincost += flow[u] * dis[u];
-        while (u != s) {
-            edge[last[u]].flow -= flow[t];
-            edge[last[u] ^ 1].flow += flow[t];
-            u = pre[u];
+int dfs(int p = s, int flow = INF)
+{
+    if (p == t)
+        return flow;
+    vis[p] = 1;
+    int rmn = flow;
+    for (int eg = cur[p]; eg && rmn; eg = edges[eg].next)
+    {
+        cur[p] = eg;
+        int to = edges[eg].to, vol = edges[eg].w;
+        if (vol > 0 && !vis[to] && dis[to] == dis[p] + edges[eg].c)
+        {
+            int c = dfs(to, min(vol, rmn));
+            rmn -= c;
+            edges[eg].w -= c;
+            edges[eg ^ 1].w += c;
         }
     }
-    return {maxflow, mincost};
+    vis[p] = 0;
+    return flow - rmn;
 }
-
-void init(int n){
-	num = 0;
-    for (int i = 1; i <= n; ++i) head[i] = -1;
-}
-
-int main() {
-    int n, m, s, t;
-    read(n, m, s, t);
-    for (int i = 0; i != m; ++i) {
-        int x, y, w, f;
-        read(x, y, w, f);
-        add2edge(x, y, w, f);
+int maxflow, mincost;
+inline void run(int s, int t)
+{
+    MCMF::s = s, MCMF::t = t;
+    while (SPFA())
+    {
+        int flow = dfs();
+        maxflow += flow;
+        mincost += dis[t] * flow;
     }
-    auto res = dinic(n, s, t);
-    printf("%d %d\n", res.first, res.second);
-    return 0;
 }
+} // namespace MCMF
